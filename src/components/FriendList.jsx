e8ae35996f5af2bc8185cc3004ecef077174382a
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-24 15:45:28 +0300

    fix

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -4,18 +4,10 @@ export const FriendList = ({ friends }) => {
     return (<ul className="friend-list">
         {friends.map((el) => {
       return <li className="item" key={el.id}>
-          {el.isOnline ? <span className="status" style={{
-              display: 'block',
+          {el.isOnline ? <span className="status-friendList" style={{
               backgroundColor: 'green',
-              borderRadius: '50%',
-              width: '20px',
-              height: '20px'
-          }}></span> : <span className="status" style={{
-              display: 'block',
-              borderRadius: '50%',
-              backgroundColor: 'red',
-              width: '20px',
-              height: '20px'}}></span>}
+          }}></span> : <span className="status-friendList" style={{
+              backgroundColor: 'red',}}></span>}
   <img className="avatar" src={el.avatar} alt="User avatar" width="48" />
           <p className="name">{el.name}</p>
 </li>
